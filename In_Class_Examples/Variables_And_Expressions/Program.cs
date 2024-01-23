e0067da835de51adcebcf2444775f5a0408b5f21
Ackerman, Adam P. <adam@ou.edu>
2024-01-23 11:44:37 -0600

    Completed the first problem of variables and expressions

--- In_Class_Examples/Variables_And_Expressions/Program.cs
+++ In_Class_Examples/Variables_And_Expressions/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Welcome the user to your application
+//and prompt the user to enter their name.
+//
+//Prompt the user, while using their name,
+//for their total monthly income as well as
+//their total monthly expenses.
+//
+//Output the difference between the two values.
+
+
+//Console.WriteLine("Welcome to my application!\nWhat is your name? >>");
+Console.WriteLine("Welcome to my application!");
+
+Console.WriteLine("What is your name? >>");
+string firstName = Console.ReadLine();
+
+//Console.WriteLine($"{firstName}, what is your monthly income? >>");
+Console.WriteLine(firstName + ", what is your monthly income? >>");
+string monthlyIncomeAsString = Console.ReadLine();
+double monthlyIncome = Convert.ToDouble(monthlyIncomeAsString);
+
+Console.WriteLine($"{firstName}, what is your monthly expenses? >>");
+double monthlyExpenses = Convert.ToDouble(Console.ReadLine());
+
+double netTotal = monthlyIncome - monthlyExpenses;
+
+Console.WriteLine($"The difference in income and expenses is {netTotal.ToString("C10")}");
