69309fb6a7d8f237f02fb30a7b343f92068fb657
Ackerman, Adam P. <adam@ou.edu>
2024-01-30 11:00:51 -0600

    Completed the SumOf3 participation

---
+++ Participations/SumOf3/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+Console.WriteLine("Please enter your first number >>");
+string answer = Console.ReadLine();
+double val1 = Convert.ToDouble(answer);
+
+Console.WriteLine("Please enter your second number >>");
+answer = Console.ReadLine();
+double val2 = Convert.ToDouble(answer);
+
+Console.WriteLine("Please enter your third number >>");
+answer = Console.ReadLine();
+double val3 = Convert.ToDouble(answer);
+
+double total = val1 + val2 + val3;
+
+Console.WriteLine($"The sum of your numbers\n{val1.ToString("N3")} + {val2.ToString("N3")} + {val3.ToString("N3")} = {total.ToString("N3")}");
+
+const double MAGIC_NUMBER = 7.777;
+
+double magicTotal = total * MAGIC_NUMBER;
+
+Console.WriteLine($"The magic total is {magicTotal.ToString("N3")}");
