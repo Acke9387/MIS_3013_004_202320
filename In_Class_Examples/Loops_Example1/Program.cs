5507448e35b757d3e01c7bcb1fe1659845f56359
Ackerman, Adam P. <adam@ou.edu>
2024-02-06 11:39:54 -0600

    completed in class examples over loops

---
+++ In_Class_Examples/Loops_Example1/Program.cs
@@ -0,0 +1,29 @@
+﻿//Ask the user to provide input for what number they want to count by as well
+//as up to which number.
+//E.g. I wish to count by 3 up to 10.
+//The output would be "3, 6, 9"
+
+Console.WriteLine("What do you want to count by? >>");
+int countBy = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("What do you want to count to? >>");
+int countTo = Convert.ToInt32(Console.ReadLine());
+
+int count = countBy;
+
+Console.WriteLine("\n\nWhile Loop Way");
+while (count <= countTo)
+{
+    Console.WriteLine(count);
+    count = count + countBy;
+    // shortened way to write the same expression
+    //count += countBy;
+}
+
+
+Console.WriteLine("\n\nFor Loop Way");
+
+for (int i = countBy; i <= countTo; i = i + countBy)
+{
+    Console.WriteLine(i);
+}
---
+++ In_Class_Examples/Loops_Example3/Program.cs
@@ -0,0 +1,24 @@
+﻿for (int i = 1; i <= 900; i++)
+{
+
+    //see if it's divisible by 3 and 5 - 'Fizzbuzz'
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("Fizzbuzz");
+    }
+    //see if it's divisible by 3 - 'Fizz'
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    //see if it's divisible by 5 - 'Buzz'
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    //output the number
+    else
+    {
+        Console.WriteLine(i.ToString("N0"));
+    }
+}
