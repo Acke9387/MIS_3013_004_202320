d857b5de0f5733797b08942b8ce031efdc1175af
Ackerman, Adam P. <adam@ou.edu>
2024-02-06 11:08:41 -0600

    Completed the coin toss problem

---
+++ Participations/CoinToss/Program.cs
@@ -0,0 +1,44 @@
+﻿Random rand = new Random();
+int randomNumber = rand.Next(1, 3);
+
+// for testing purposes
+//Console.WriteLine(randomNumber);
+
+const int HEADS = 1;
+const int TAILS = 2;
+const string DEVELOPER_NAME = "Professor Ackerman";
+
+Console.WriteLine("Guess heads or tails? >>");
+string usersGuess = Console.ReadLine();
+
+int usersGuessAsNumber;
+
+if (randomNumber == HEADS)
+{
+    Console.WriteLine("The coin came up heads.");
+}
+else
+{
+    Console.WriteLine("The coin came up tails.");
+}
+
+if (usersGuess == "heads")
+{
+    usersGuessAsNumber = HEADS;
+}
+else
+{
+    usersGuessAsNumber = TAILS;
+}
+
+if (usersGuessAsNumber == randomNumber)
+{
+    Console.WriteLine("Congratulations, you guessed correct!");
+}
+else
+{
+    Console.WriteLine("Sorry, you guessed wrong!");
+}
+
+
+Console.WriteLine($"This application was created by {DEVELOPER_NAME}.");
