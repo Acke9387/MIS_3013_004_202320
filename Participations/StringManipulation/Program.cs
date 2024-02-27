7f1e8eea7a653cf46659b75d87b02500005bd529
Ackerman, Adam P. <adam@ou.edu>
2024-02-27 11:06:45 -0600

    completed participation over string manipulation

---
+++ Participations/StringManipulation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+string sentence = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.";
+
+// Output to the user the starting sentence
+Console.WriteLine(sentence);
+
+Console.WriteLine("Please give me a word that you want to change from the sentence >>");
+string searchWord = Console.ReadLine();
+
+Console.WriteLine($"What do you want to change {searchWord} to? >>");
+string changeWord = Console.ReadLine();
+
+
+if (sentence.Contains(searchWord) == true)
+{
+    Console.WriteLine($"Sentence contains {searchWord}");
+    string correctedSentence = sentence.Replace(searchWord, changeWord);
+    Console.WriteLine(correctedSentence);
+}
+else
+{
+    Console.WriteLine($"Sorry, I could not find your word {searchWord}.");
+
+    for (int i = searchWord.Length - 1; i >= 0; i--)
+    {
+        Console.Write(searchWord[i]);
+    }
+    Console.WriteLine();
+
+
+}
