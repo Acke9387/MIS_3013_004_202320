198a3187cb07829423123e87cc56b8cef82997fc
Ackerman, Adam P. <adam@ou.edu>
2024-02-29 16:29:50 -0600

    did in class example over list

---
+++ In_Class_Examples/Collections_List/Program.cs
@@ -0,0 +1,21 @@
+﻿List<string> favoriteFruits = new List<string>();
+
+favoriteFruits.Add("banana");
+favoriteFruits.Add("grapes");
+favoriteFruits.Add("oranges");
+Console.WriteLine("My favorite fruits are: ");
+
+
+//for (int i = 0; i < favoriteFruits.Count; i++)
+//{
+//    favoriteFruits[i] = favoriteFruits[i] + ".";
+//    Console.WriteLine(favoriteFruits[i]);
+//}
+
+foreach (string fruit in favoriteFruits)
+{
+	if (fruit.ToLower()[0] == 'b')
+	{
+		Console.WriteLine(fruit); 
+	}
+}
