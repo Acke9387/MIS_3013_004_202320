88056af668308f45a2e44be8554948b7b5095f05
Ackerman, Adam P. <adam@ou.edu>
2024-04-11 16:59:49 -0500

    COmpletd the toy participation

---
+++ Participations/Classes_Toy/Program.cs
@@ -0,0 +1,24 @@
+﻿using Classes_Toy;
+
+List<Toy> toys = new List<Toy>();
+
+Toy myToy = new Toy();
+myToy.Name = "Switch";
+myToy.Manufacturer = "Nintendo";
+myToy.Price = 299.99;
+myToy.SetNotes("Fun for all ages");
+
+toys.Add(myToy);
+
+//Console.WriteLine($"{myToy.Name} is made by {myToy.Manufacturer} and sells for {myToy.Price.ToString("C")}\n{myToy.GetNotes()}");
+//Console.WriteLine(myToy);
+
+Toy myToy2 = new Toy("Barbie", "Mattel", 25.25, "Perfect toy for your kid");
+
+toys.Add(myToy2);
+//Console.WriteLine(myToy2);
+
+foreach (Toy toy in toys)
+{
+    Console.WriteLine(toy);
+}
---
+++ Participations/Classes_Toy/Toy.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Toy
+{
+    public class Toy
+    {
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+
+        private double _Price;
+        public double Price { get; set; }
+
+        private string Notes;
+
+        public string GetNotes()
+        {
+            return Notes;
+        }
+        public void SetNotes(string Notes)
+        {
+            if (Notes.Length < 5)
+            {
+                return;
+            }
+            this.Notes = Notes;
+        }
+
+        /// <summary>
+        /// Default/Empty Constructor
+        /// </summary>
+        public Toy()
+        {
+            Manufacturer = string.Empty;
+            Name = string.Empty;
+            Notes = string.Empty;
+            Price = 0;
+        }
+
+        /// <summary>
+        /// Overloaded constructor
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="manu"></param>
+        /// <param name="price"></param>
+        /// <param name="note"></param>
+        public Toy(string name, string manu, double price, string note)
+        {
+            Name = name;
+            Manufacturer = manu;
+            Price = price;
+            Notes = note;
+        }
+
+
+        public string GetAisle()
+        {
+            string aisle = "";
+            Random r = new Random();
+            aisle = Manufacturer[0].ToString().ToUpper() + r.Next(1, 25);
+
+            return aisle;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} is made by {Manufacturer} and sells for {Price.ToString("C")}\n{GetNotes()}";
+
+            return "Banana";
+            return base.ToString();
+        }
+
+    }
+}
