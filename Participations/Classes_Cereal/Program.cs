a472ce1ae52ff0849bcd9ded0ae5cd809df29747
Ackerman, Adam P. <adam@ou.edu>
2024-04-11 17:05:56 -0500

    Started the cereal participation

---
+++ Participations/Classes_Cereal/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using Classes_Cereal;
+
+string[] linesOfFile = File.ReadAllLines("Cereal_Data.txt");
+
+List<Cereal> cereals = new List<Cereal>();
+
+for (int i = 1; i < linesOfFile.Length; i++)
+{
+    Cereal c = new Cereal();
+    //  0         1        2      3
+    //name|manufacturer|calories|cups 
+    string[] pieces = linesOfFile[i].Split('|');
+    c.Name = ....;
+
+    cereals.Add(c);
+}
+
+Console.WriteLine("Cereals with serving size of 1 cup or more");
+foreach (Cereal cereal in cereals)
+{
+    if ()
+    {
+
+    }
+}
+
+Console.WriteLine("Cereals with 100 calories or less per serving");
+foreach (Cereal cereal in cereals)
+{
+    if ()
+    {
+
+    }
+}
