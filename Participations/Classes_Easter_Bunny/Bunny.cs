e9508c67e0b33b974d1a5ea5fdf389a75712acac
Ackerman, Adam P. <adam@ou.edu>
2024-04-25 11:06:13 -0500

    COmpleted the easter bunny participation

---
+++ Participations/Classes_Easter_Bunny/Bunny.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Easter_Bunny
+{
+    public class Bunny
+    {
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+        public string DeliveryDate { get; set; }
+
+        public int BasketSize { get; set; }
+
+        public string EggColor { get; set; }
+
+        public int EggCount { get; set; }
+
+        public string Location { get; set; }
+        public string FavoriteColor { get; set; }
+
+        // Create an empty constructor setting all properties to default empty values
+        public Bunny()
+        {
+            Name = "";
+            Age = 0;
+            DeliveryDate = "";
+            BasketSize = 0;
+            EggColor = "";
+            EggCount = 0;
+            Location = "";
+            FavoriteColor = "";
+        }
+
+        // create an overloaded constructor that accepts the name and age of the bunny
+        public Bunny(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            DeliveryDate = "";
+            BasketSize = 0;
+            EggColor = "";
+            EggCount = 0;
+            Location = "";
+            FavoriteColor = "";
+        }
+
+        public int HowManyEasterBaskets()
+        {
+            // Calculate how many baskets the bunny will need to deliver the eggs and take the ceiling
+            int baskets = (int)Math.Ceiling((double)EggCount / BasketSize);
+            baskets = EggCount / BasketSize;
+
+
+            return baskets;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} is {Age} years old and loves the color {FavoriteColor} and will deliver {EggCount.ToString("N")} eggs on {DeliveryDate} in {Location}.";
+        }
+
+    }
+}
---
+++ Participations/Classes_Easter_Bunny/Program.cs
@@ -0,0 +1,62 @@
+﻿using Classes_Easter_Bunny;
+
+string[] lines = File.ReadAllLines("Easter_Bunny.csv");
+
+List<Bunny> bunnies = new List<Bunny>();
+
+for (int i = 1; i < lines.Length; i++)
+{
+    //Name,Age,FavoriteColor,Location,EggCount,EggColor,BasketSize,DeliveryDate
+    string[] pieces = lines[i].Split(',');
+    string name = pieces[0];
+    //...
+
+    Bunny b = new Bunny(pieces[0], Convert.ToInt32(pieces[1]));
+    //b.Name = pieces[0];
+    //b.Age = int.Parse(pieces[1]);
+    b.FavoriteColor = pieces[2];
+    b.Location = pieces[3];
+    b.EggCount = Convert.ToInt32(pieces[4]);
+    b.EggColor = pieces[5];
+    b.BasketSize = Convert.ToInt32(pieces[6]);
+    b.DeliveryDate = pieces[7];
+
+    bunnies.Add(b);
+}
+int totalEgg = 0;
+foreach (Bunny b in bunnies)
+{
+    totalEgg += b.EggCount;
+}
+Console.WriteLine($"The total eggs from all Bunnies is {totalEgg.ToString("N")}");
+
+
+Console.WriteLine($"\nAll the bunnies that are older than 75 are :");
+
+foreach (Bunny b in bunnies)
+{
+    if (b.Age > 75)
+    {
+        Console.WriteLine("\t" + b);
+    }
+}
+
+foreach (Bunny b in bunnies)
+{
+    //output where age is less than 20
+    if (b.Age < 20)
+    {
+        Console.WriteLine("\t" + b);
+    }
+}
+
+Console.WriteLine("\nWhat color do you want to check for the Bunnies Favorite color? >>");
+string color = Console.ReadLine();
+
+foreach (Bunny b in bunnies)
+{
+    if (b.FavoriteColor.ToLower() == color.ToLower())
+    {
+        Console.WriteLine("\t" + b);
+    }
+}
